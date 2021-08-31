736279768cb44c573ff99b036259d99c940874f4
Isuru Adikaram <izuru775@gmail.com>
2021-08-31 22:24:59 +1000

    modify server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.use(express.static(__dirname + '/public'))
+
 
 app.get('/',(req,res,next)=>{
-    res.send('<h1>Hello</h1>')
+    res.sendFile(path.goin(__dirname,'public','index.html'))
 })
 
 app.listen(5000)
