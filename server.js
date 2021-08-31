eec0e3f05a01d040f6024424cde0e83ee3a5d1d4
Isuru Adikaram <izuru775@gmail.com>
2021-08-31 22:52:34 +1000

    add PORT

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const PORT = process.env.PORT || 5000
+
 app.use(express.static(__dirname + '/public'))
 
 
@@ -10,4 +12,4 @@ app.get('/',(req,res,next)=>{
     res.sendFile(path.goin(__dirname,'public','index.html'))
 })
 
-app.listen(5000)
+app.listen(PORT,console.log(`server started on port${PORT}`))
