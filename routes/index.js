94dd01c2cab42262c641d5500e102c22ac41c67c
Isuru Adikaram <izuru775@gmail.com>
2021-08-31 23:12:28 +1000

    add routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+// Login Page
+router.get('/Login',(req,res,next)=>{
+    res.send('Login')
+})
+// Register Page 
+router.get('/Register',(req,res,next)=>{
+    res.send('Register')
+})
+
+module.exports = router
