9584e76677470be1edbfcf6d214a26ef3bcc7913
Isuru Adikaram <izuru775@gmail.com>
2021-09-01 23:39:27 +1000

    Render welcome.ejs added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Hello again!'))
+router.get('/', (req, res) => {
+    res.render('welcome')
+})
 
 module.exports = router
