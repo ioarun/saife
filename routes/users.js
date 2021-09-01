e58504db5461cb0677bdaf92e4db587ffce723a9
Isuru Adikaram <izuru775@gmail.com>
2021-09-01 23:40:13 +1000

    Render login.ejs & register.ejs added to routes

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 // Login Page
 router.get('/login', (req, res) => {
-    res.send('Login')
+    res.render('login')
 })
 // Register Page 
 router.get('/register', (req, res) => {
-    res.send('Register')
+    res.render('register')
 })
 
 module.exports = router;
