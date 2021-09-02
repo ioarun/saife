f28211c4f33041dad56490a203570c2f979f095e
Isuru Adikaram <izuru775@gmail.com>
2021-09-02 20:10:56 +1000

    Express-session, Connect-flash middleware added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose')
+const flash = require('connect-flash') // Connect flash
+const session = require('express-session') // Connect session
 
 const app = express()
 
@@ -19,6 +21,23 @@ app.set('view engine', 'ejs');
 
 // Bodyparser (can get data from form using request.body)
 app.use(express.urlencoded({ extended: true }))
+
+// Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+
+// Connect flash
+app.use(flash())
+
+// Global Vars
+app.use((req,res,next)=>{
+    res.locals.success_msg =req.flash('success_msg');
+    res.locals.error_msg =req.flash('error_msg');
+    next()
+})
 
 const PORT = process.env.PORT || 5000
 
