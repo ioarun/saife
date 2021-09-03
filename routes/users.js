6f1b63a15a199ba851ea9eb1d9e1a2ee5be851ff
Isuru Adikaram <izuru775@gmail.com>
2021-09-03 12:24:22 +1000

    Logout handle created

--- routes/users.js
+++ routes/users.js
@@ -91,10 +91,18 @@ router.post('/register', (req, res) => {
 // Login handle
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', {
-      successRedirect: '/dashboard',
-      failureRedirect: '/users/login',
-      failureFlash: true
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
     })(req, res, next);
-  });
+});
+
+
+// Logout handle
+router.get('/logout', (req, res) => {
+    req.logout()
+    req.flash('success_msg','You are logged out')
+    res.redirect('/users/login')
+})
 
 module.exports = router;
