d6b52152b8beb9386592f16a17e606e5f1d3ab75
Isuru Adikaram <izuru775@gmail.com>
2021-09-03 12:24:03 +1000

    Route to dashboard created

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// Welcome Page
 router.get('/', (req, res) => {
     res.render('welcome')
+})
+// Dashboard
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
 })
 
 module.exports = router
