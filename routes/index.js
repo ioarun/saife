3f591c0bd8bee12413f6d945e690bbe3ae417aa4
Isuru Adikaram <izuru775@gmail.com>
2021-09-03 14:47:00 +1000

    User authentication added

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const { ensureAuthenticated } = require('../config/auth');
 // Welcome Page
 router.get('/', (req, res) => {
-    res.render('welcome', {title: "Welcome"})
+    res.render('welcome', { title: "Welcome" })
 });
 // // Wellcome Page
 // router.get('/',(req,res,next)=>{
 //     // res.send('Welcome')
 //     res.render("layout", {title: "MAIN"});
 // })
-
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', { title: "Dashboard", name: req.user.name })
+});
 module.exports = router
