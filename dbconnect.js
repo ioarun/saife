f6fee98127b1852c4bcd699f25a8c0e277ab5a2d
Isuru Adikaram <izuru775@gmail.com>
2021-09-04 19:01:33 +1000

    Minor changes

--- dbconnect.js
+++ dbconnect.js
@@ -5,11 +5,16 @@ const db = require('./config/keys').MongoURI
 
 // Connect to mongo
 
-const mongo = ()=>{
-mongoose.connect(db, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB connected....'))
-    .catch(err => console.log(err))
-} 
+const mongo = () => {
+    mongoose.connect(db, { useNewUrlParser: true })
+        .then(() => console.log('MongoDB connected....'))
+        .catch(err => console.log(err))
+}
+
+const close = () => {
+    mongoose.disconnect()
+}
+
+module.exports = mongo,close
 
 
-module.exports = mongo
