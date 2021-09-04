ff5e9246ced46a77880084c78d42f451f6acba16
Isuru Adikaram <izuru775@gmail.com>
2021-09-04 21:04:21 +1000

    Fixed the redirect on login just  to root

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome Page
-router.get('/', (req, res) => {
-    res.render('welcome', { title: "Welcome" })
-});
+// router.get('/', (req, res) => {
+//     res.render('welcome', { title: "Welcome" })
+// });
 // // Wellcome Page
 // router.get('/',(req,res,next)=>{
 //     // res.send('Welcome')
 //     res.render("layout", {title: "MAIN"});
 // })
-router.get('/dashboard', ensureAuthenticated, (req, res) => {
+router.get('/', ensureAuthenticated, (req, res) => {
     let fullname = req.user.firstname + " " + req.user.lastname
     res.render('dashboard', { title: "Dashboard", name: fullname })
 });
--- routes/users.js
+++ routes/users.js
@@ -105,7 +105,7 @@ router.post('/register', (req, res) => {
 // Login handle
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', {
-        successRedirect: '/dashboard',
+        successRedirect: '/',
         failureRedirect: '/users/login',
         failureFlash: true
     })(req, res, next);
