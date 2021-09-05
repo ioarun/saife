f504bd25915f53ea67f3f0eb8c4182a96113a152
nandugangavaram <nandugangavaram@gmail.com>
2021-09-05 12:34:24 +0530

    Add new members functionality with reload

--- routes/members.js
+++ routes/members.js
@@ -1,26 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-// Members Page
+//members array
+var members = [
+    {
+        firstName: "Gouri Nandan",
+        status: "Fallen!"
+    },
+    {
+        firstName: "Arun",
+        status: "All Good!"
+    },
+    {
+        firstName: "Sodi",
+        status: "Fallen!"
+    },
+    {
+        firstName: "Chasham",
+        status: "All Good!"
+    }
+];
+
+// Get Members
 router.get('/', (req, res) => {
-    let members = [
-        {
-            name: "Gouri Nandan",
-            status: "Fallen!"
-        },
-        {
-            name: "Arun",
-            status: "All Good!"
-        },
-        {
-            name: "Sodi",
-            status: "All Good!"
-        },
-        {
-            name: "Chasham",
-            status: "All Good!"
-        }
-    ];
+    console.log(members);
+    res.render('members', {title: "Members", members});
+});
+
+// Add Member
+router.post('/addMember', (req, res) => {
+    let member = req.body;
+    member.status = "All Good!";
+    members.push(member);
     res.render('members', {title: "Members", members});
 });
 
