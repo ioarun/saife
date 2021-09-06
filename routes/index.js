3b37a70f8937793f17a96f0673ba13009dd93456
Isuru Adikaram <izuru775@gmail.com>
2021-09-06 11:08:06 +1000

    Minor changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
+let members = [
+    {
+        name: "Gouri Nandan",
+        age: "38"
+    },
+    {
+        name: "Arun",
+        age: "36"
+    },
+    {
+        name: "Sodi",
+        age: "35"
+    },
+    {
+        name: "Chasham",
+        age: "33"
+    }
+];
 
 // Welcome Page
 // router.get('/', (req, res) => {
@@ -13,6 +31,6 @@ const { ensureAuthenticated } = require('../config/auth');
 // })
 router.get('/', ensureAuthenticated, (req, res) => {
     let fullname = req.user.firstname + " " + req.user.lastname
-    res.render('dashboard', { title: "Dashboard", name: fullname })
+    res.render('members', { title: "Dashboard", name: fullname ,members})
 });
 module.exports = router
