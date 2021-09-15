5e12673f15f867551ff8cc81412f54813269aeb1
nandugangavaram <nandugangavaram@gmail.com>
2021-09-15 18:11:04 +0530

    MyMembers unwanted routes removed

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.use(express.static(path.join(__dirname, 'views')))
 // Routes
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
-app.use('/myMembers', require('./routes/members.js'));
+// app.use('/myMembers', require('./routes/members.js'));
 
 
 app.listen(PORT,console.log(`server started on port ${PORT}`));
