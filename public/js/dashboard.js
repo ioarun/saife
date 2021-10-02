b8b71345697d5db172f8f63cc24a944308c61601
izuru775 <izuru775@gmail.com>
2021-10-03 01:34:37 +1000

    Repeated serviceworker.register deleted

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,22 +5,6 @@ function isPushSupported() {
 //To check `push notification` permission is denied by user
 if (Notification.permission === 'denied') {
 //   alert('User has blocked push notification.');
-
-navigator.serviceWorker.register('/worker.js');
-    navigator.serviceWorker.ready
-        .then(function (registration) {
-            console.log('service worker registered');
-            subscriptionButton.removeAttribute('disabled');
-
-            return registration.pushManager.getSubscription();
-        }).then(function (subscription) {
-            if (subscription) {
-                console.log('Already subscribed', subscription.endpoint);
-                setUnsubscribeButton();
-            } else {
-                setSubscribeButton();
-            }
-        });
   Swal.fire(
     'Alert',
     'User has blocked push notification.',
